05aed3e321eade16d93d26bcfa4bb15132210597
Airat Galiev <airat.galiev@gmail.com>
2020-12-03 21:30:57 +0300

    created new project

---
+++ src/test/java/com/github/airatgaliev/currencyconversionservice/CurrencyConversionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.airatgaliev.currencyconversionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConversionServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
